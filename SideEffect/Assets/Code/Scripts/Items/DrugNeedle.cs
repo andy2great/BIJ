daddfea462a3d326dfba42fe7869d82c21ad1cea
Matthieu Pauze <matthieu.pauze@beslogic.com>
2023-01-29 17:50:51 -0500

    balance item stats

--- SideEffect/Assets/Code/Scripts/Items/DrugNeedle.cs
+++ SideEffect/Assets/Code/Scripts/Items/DrugNeedle.cs
@@ -8,7 +8,7 @@ public class DrugNeedle : BaseItem
   public override string Name { get; set; } = "DrugNeedle";
   public override int Damage { get; set; } = 3;
   public override int Health { get; set; } = 1;
-  public override float XSpeedFactor { get; set; } = 2f;
+  public override float XSpeedFactor { get; set; } = 1.25f;
   public override float YSpeedFactor { get; set; } = 0.5f;
   public override float Spin { get; set; } = 0f;
 
--- SideEffect/Assets/Code/Scripts/Items/TearGas.cs
+++ SideEffect/Assets/Code/Scripts/Items/TearGas.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class TearGas : BaseItem
 {
   public override string Name { get; set; } = "TearGas";
-  public override int Damage { get; set; } = 10;
+  public override int Damage { get; set; } = 5;
   public override int Health { get; set; } = 1;
   public override float XSpeedFactor { get; set; } = 0.5f;
-  public override float YSpeedFactor { get; set; } = 4f;
+  public override float YSpeedFactor { get; set; } = 1f;
   public override float Spin { get; set; } = 1.5f;
 
   public override Type[] EffectTypes => new Type[] { typeof(Blinking) };
