ff3f5299978bcd37a6de1641ae2345e1d7c7c6a1
Matthieu Pauze <matthieu.pauze@beslogic.com>
2023-01-28 19:17:22 -0500

    allow to drop through platforms

--- SideEffect/Assets/Code/Scripts/Player.cs
+++ SideEffect/Assets/Code/Scripts/Player.cs
@@ -32,17 +32,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    void OnTriggerStay2D(Collider2D collision)
-    {
-        if ((Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.E)) && collision.gameObject.tag == "Item")
-        {
-            Throw(new Vector2(0, 7f));
-
-            Item = collision.gameObject.GetComponent<BaseItem>();
-            Item.PickUpItem();
-        }
-    }
-
     private void Move() {
         if (Input.GetAxis("Horizontal") != 0) {
             FacingRight = Input.GetAxis("Horizontal") > 0;
@@ -52,7 +41,7 @@ public class Player : MonoBehaviour
         
         if (Input.GetButtonDown("Jump") && Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) < 0.001f && !(Input.GetAxis("Vertical") < 0))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 10f), ForceMode2D.Impulse);
         }
 
         transform.rotation = Quaternion.identity;
@@ -84,16 +73,5 @@ public class Player : MonoBehaviour
             Item = collision.gameObject.GetComponent<BaseItem>();
             Item.PickUpItem();
         }
-        Debug.Log(collision.gameObject.tag);
-        if (collision.gameObject.tag == "Platform" && Input.GetButtonDown("Jump") && Input.GetAxis("Vertical") < 0) {
-            StartCoroutine(FallThroughFloor());
-        }
-    }
-
-    IEnumerator FallThroughFloor() {
-        Debug.Log("Should fall");
-        GetComponent<Collider2D>().enabled = false;
-        yield return new WaitForSeconds(0.25f);
-        GetComponent<Collider2D>().enabled = true;
     }
 }
